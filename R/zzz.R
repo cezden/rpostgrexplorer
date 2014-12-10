d9ff7a31f8f62b1e5e435b9caa9bce93f4e846da
Cezary Dendek <cezary.dendek@taac.im>
2014-12-10 15:54:21 +0100

    during #3, ability to comment in the SQL files

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,16 @@
 .onLoad <- function(libname, pkgname) {
   assign("pkg_globals", new.env(), envir=parent.env(environment()))
-  sql.files <- c("tables_list", "table_attributes_list")
+  sql.files <- c("tables_list", "table_attributes_list", "relationship_miner")
   for (sqlfile in sql.files){
     sqlfile.name <- paste0(sqlfile,".sql")
-    vvv <- readLines(system.file("sql", sqlfile.name, package = pkgname))    
-    assign(sqlfile, paste(vvv, collapse = "\n"), pkg_globals)
+    sqlfile.contents <- readLines(system.file("sql", sqlfile.name, package = pkgname))    #read sql file content
+    #remove comments
+    sqlfile.contents.filtered <- Filter(
+      function(x){
+        !(stringi::stri_startswith_fixed(stringi::stri_trim_left(x), "--"))
+      }
+      , sqlfile.contents)
+    final.sql <- paste(sqlfile.contents.filtered, collapse = "\n")
+    assign(sqlfile, final.sql, pkg_globals)
   }
 }
--- inst/sql/tables_list.sql
+++ inst/sql/tables_list.sql
@@ -1,3 +1,13 @@
+-- #' Constructs the query retrieving the metadata information concerning the tables:
+-- #' \itemize{
+-- #'    \item{schemaname}
+-- #'    \item{tablename}
+-- #'    \item{number of attributes}
+-- #'    \item{estimate of counts}
+-- #'    \item{is table indexed?}
+-- #'    \item{has table a primary key?}
+-- #' } 
+
 select 
   t.schemaname, t.tablename, 
   c.relnatts as attributenumber, c.reltuples as count_estimate, c.relhasindex as has_index, c.relhaspkey as has_primary_key
