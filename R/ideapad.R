849e37b6f742e0ed90f8f5040dc3b01082d6dda2
Cezary Dendek <cezary.dendek@taac.im>
2014-12-17 18:58:06 +0100

    * experimental.infer.domains

--- R/ideapad.R
+++ R/ideapad.R
@@ -33,3 +33,32 @@ experimental.db.sketch.attribute <- function(db.connection, metadata.inf, attrib
   query.load.execute(querylist, db.connection)
 }
 
+#' Inference of attribute domains
+#'
+#' Function searches for the frequently co-occuring sets of attributes
+#' 
+#' @param metadata.inf object of S3 class \code{\link{metadata.info}}
+#' @param minimal.support the minimal support (among the tables) for the domain
+#' @param maximal.length the maximal number of attributes in the domain
+#' @return object of class \code{\link[arules]{itemsets}} from the \emph{arules} package
+#' @export
+#' @importFrom dplyr %>%
+#' @importClassesFrom arules transactions itemsets
+experimental.infer.domains <- function(meta.inf, minimal.support = 0.1, maximal.length = 20){
+  schemed.attribs <- db.attributes(meta.inf) %>% dplyr::mutate(schema.tab = paste0(schemaname, ".", tablename))
+  schemed.attribs.trans <- as(split(schemed.attribs[, "attname"], schemed.attribs[, "schema.tab"]), "transactions")
+  freq.sets <- arules::eclat(schemed.attribs.trans, parameter = list(supp = minimal.support, maxlen = maximal.length))
+  # infering maximal
+  support.vals <- arules::quality(freq.sets)$support
+  support.vals.unique <- sort(unique(support.vals))
+  leveled.maximals <- lapply(
+    support.vals.unique, 
+    function(x){
+      lev.rules <- freq.sets[support.vals==x]
+      lev.rules[arules::is.maximal(lev.rules)]
+    }
+  )
+  maximal.rules <- Reduce(arules::union, leveled.maximals[-1], leveled.maximals[[1]])
+  arules::sort(maximal.rules)
+}
+
