34e58f460a68f2e6aafe5413fdb0685d83638647
Cezary Dendek <cezary.dendek@gmail.com>
2014-12-19 04:08:20 +0100

    * new idea

--- R/ideapad.R
+++ R/ideapad.R
@@ -62,3 +62,46 @@ experimental.infer.domains <- function(meta.inf, minimal.support = 0.1, maximal.
   arules::sort(maximal.rules)
 }
 
+#' Inference of attribute domains
+#'
+#' Function searches for the frequently co-occuring sets of attributes
+#' 
+#' @param metadata.inf object of S3 class \code{\link{metadata.info}}
+#' @param domain.set set of domains
+#' @param minimal.support the minimal support (among the tables) for the domain
+#' @param maximal.length the maximal number of attributes in the domain
+#' @return object of class \code{\link[arules]{itemsets}} from the \emph{arules} package
+#' @export
+#' @importFrom dplyr %>%
+#' @importClassesFrom arules transactions itemsets
+experimental.infer.domains2 <- function(meta.inf, domain.set = list(), minimal.support = 0.1, maximal.length = 20){
+  schemed.attribs <- db.attributes(meta.inf) %>% dplyr::mutate(schema.tab = paste0(schemaname, ".", tablename))
+  schemed.attribs.trans.pure <- split(schemed.attribs[, "attname"], schemed.attribs[, "schema.tab"])
+  schemed.attribs.trans.red <- lapply(
+    schemed.attribs.trans.pure, function(x){
+      for(domain in names(domain.set)){
+        if (all(domain.set[[domain]] %in% x)){
+          x <- c(setdiff(x, domain.set[[domain]]), domain)
+        }
+      }
+      x
+    }
+    )
+  schemed.attribs.trans <- as(schemed.attribs.trans.red, "transactions")
+  ## TODO: consider other options (the param named "parameter")
+  freq.sets <- arules::eclat(schemed.attribs.trans, 
+                             parameter = list(supp = minimal.support, maxlen = maximal.length, minlen = 2))
+  #for each level of support select elements that (on the group) get is.maximal==TRUE
+  support.vals <- arules::quality(freq.sets)$support
+  support.vals.unique <- sort(unique(support.vals))
+  leveled.maximals <- lapply(
+    support.vals.unique, 
+    function(x){
+      lev.rules <- freq.sets[support.vals==x]
+      lev.rules[arules::is.maximal(lev.rules)]
+    }
+  )
+  maximal.rules <- Reduce(arules::union, leveled.maximals[-1], leveled.maximals[[1]])
+  arules::sort(maximal.rules)
+}
+
---
+++ R/meta_objects.R
@@ -0,0 +1,24 @@
+to.define <- function(){
+
+db.attrib <- function(attribute.spec, attribute.type = NULL){
+  
+}
+
+op.all <- function(){
+  
+}
+
+op.some <- function(min.number=1){
+  
+}
+
+op.none <- function(){
+  
+}
+
+restrict <- function(){
+  
+}
+
+
+}
