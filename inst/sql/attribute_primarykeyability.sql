419b25cf1aaed153e2c1d674764237d03497022d
Cezary Dendek <cezary.dendek@taac.im>
2014-12-23 16:28:54 +0100

    * SQL query for extracting entropy of attribute

--- inst/sql/attribute_primarykeyability.sql
+++ inst/sql/attribute_primarykeyability.sql
@@ -1,12 +1,24 @@
-select 
+select
+  row_cnt,
+	distinct_val_cnt,
+	mincnt,
+	avgcnt,
+	maxcnt,
+	sdevcnt,
+	null_cnt,
+	log(row_cnt) - entr_pre/row_cnt as entropy
+from
+(
+  select
         sum(%%VALCOUNTER%%) as row_cnt, 
         count(*) as distinct_val_cnt, 
         min(%%VALCOUNTER%%) as mincnt, 
         avg(%%VALCOUNTER%%) as avgcnt, 
         max(%%VALCOUNTER%%) as maxcnt, 
         stddev_samp(%%VALCOUNTER%%) as sdevcnt,
-        sum(%%NULLITYMARKER%% * %%VALCOUNTER%%) as null_cnt 
-        from
+        sum(%%NULLITYMARKER%% * %%VALCOUNTER%%) as null_cnt,
+        sum(cnt::real*log(cnt::real)) as entr_pre
+  from
         (select 
             %%VALNAME%%, 
             count(*) as %%VALCOUNTER%%, 
@@ -17,4 +29,4 @@ select
           from 
             (%%QUERY%%) 
           group by 
-            %%VALNAME%%)
+            %%VALNAME%%))
