d999f76f1f57c4c7beb097f0e9ed9684fd30af0d
Cezary Dendek <cezary.dendek@gmail.com>
2015-01-07 19:08:05 +0100

    * dictionary improvement

--- R/dictionaries.R
+++ R/dictionaries.R
@@ -1,40 +1,104 @@
 
+#@export
+#semantics.dictionary <- function(df.db.table, v.key.field, v.value.field){
+#  list(db.tables = df.db.table, db.key.cols = v.key.field, db.val.cols = v.value.field)
+#}
+
 #' @export
-experimental.db.dictionary <- function(df.db.table, v.key.field, v.value.field){
-  list(db.tables = df.db.table, db.key.cols = v.key.field, db.val.cols = v.value.field)
+semantics.dictionary <- function(df.db.table, key.field, value.field){
+  z <- list(
+    db.table = df.db.table, 
+    key.cols = key.field, 
+    val.cols = value.field, 
+    keys.v = NA)    
+  class(z) <- "semantics.dictionary"
+  z
 }
 
 #' @export
-experimental.db.dictionary.tab <- function(df.db.table, key.field, value.field){
-  z <- list(db.table = df.db.table, key.cols = key.field, val.cols = value.field, keys.v = NA)    
-  class(z) <- "db.dictionary.tab"
-  z
+semantics.dictionary.simple <- function(meta.info, tab.name, key.attribute.name, value.attribute.name){
+  
+  dbt <- db.tables(meta.inf)
+  dba <- db.attributes(meta.inf)
+  ## DIRTY, REFINE
+  lapply(1:length(tab.name), function(x){
+    tbl <- dbt %>% filter(tablename == tab.name[x])
+    tbl.key <- dba %>% filter(tablename == tab.name[x]) %>% filter(attname == key.attribute.name[x])
+    tbl.val <- dba %>% filter(tablename == tab.name[x]) %>% filter(attname == value.attribute.name[x])
+    if (any(nrow(tbl) == 0, nrow(tbl.key) == 0, nrow(tbl.val) == 0)){
+      print(tab.name[x])
+      print(key.attribute.name[x])
+      print(value.attribute.name[x])
+      stop("Error in mapping!")
+    }
+    semantics.dictionary(tbl, tbl.key, tbl.val)    
+  })
+  
 }
+
+#' @export
+semantics.dictionary.set <- function(dictlist){
+  #reindexing by internal idx
+  internalidx <- unlist(lapply(dictlist, function(x){
+    x$db.table$internalid
+  }))
+  names(dictlist) <- internalidx
+  zz <- list(dictlist = dictlist)
+  class(zz) <- "semantics.dictionary.set"
+  zz
+}
+
+
+
+#' @export
+semantics.dictionary.set.simple <- function(meta.info, tab.name, key.attribute.name, value.attribute.name){
+  semantics.dictionary.set(semantics.dictionary.simple(meta.info, tab.name, key.attribute.name, value.attribute.name))  
+}
+
+
 
 
 #' @export
 db.query.value <- function(x, ...) UseMethod("db.query.value")
 
 
-query.dictionary.tab.value.generic <- function(dictkey, query){
-  stringi::stri_replace_all_fixed(
-    get("dictionary_values", pkg_globals), 
-    c(
-      "%%DICTKEY%%", "%%QUERY%%"
-    ), 
-    c(
-      dictkey, query
-    ),
-    vectorize_all = FALSE)    
+query.semantics.dictionary.value.generic <- function(dictkey, query){
+  sql.fill.template(
+    "dictionary_values", 
+    query.names = query, 
+    list(
+      "%%DICTKEY%%" = dictkey,
+      "%%QUERY%%" = query
+      )
+    )
 }
 
 
 #' @export
-db.query.value.db.dictionary.tab <- function(db.dictionary.obj){
-  
-  
-  db.dictionary.obj$keys.v <- 
-  "select %%KEY%% as dictkey, count(*) as dictkeycount from %%VVV%%% group by %%KEY%%"
-  db.dictionary.obj
+db.query.value.semantics.dictionary <- function(semantics.dictionary.obj, control.connection){
+  semantics.dictionary.obj$keys.v <- query.load.execute(
+    queries = query.semantics.dictionary.value.generic(
+      dictkey = semantics.dictionary.obj$key.cols$attname, 
+      query = sql.select.table(semantics.dictionary.obj$db.table)
+      ), 
+    control.connection)[[1]]
+  semantics.dictionary.obj
+}
+
+#' @export
+db.query.value.semantics.dictionary.set <- function(semantics.dictionary.set.obj, control.connection){
+  semantics.dictionary.set.obj$dictlist <- lapply(semantics.dictionary.set.obj$dictlist, function(x){
+    db.query.value(x, control.connection)
+  })
+  ## generating common value set
+  ### DIRTY, TODO: rewrite
+  common.df <- NULL
+  for(dictn in names(semantics.dictionary.set.obj$dictlist)){
+    common.df <- rbind(common.df, data.frame(semantics.dictionary.set.obj$dictlist[[dictn]]$keys.v, dictname = dictn))
+  }
+  semantics.dictionary.set.obj$dictkeys <- common.df
+  collisions.df <- common.df %>% dplyr::group_by(dictkey) %>% dplyr::summarize(cnt=n()) %>% dplyr::filter(cnt>1)
+  semantics.dictionary.set.obj$collidingkeys <- collisions.df
+  semantics.dictionary.set.obj
 }
 
--- R/meta_objects.R
+++ R/meta_objects.R
@@ -29,4 +29,54 @@ rule.matcher <- function(){
 
 }
 
+#' Constructor for class db.attribute
+#' 
+#' @param schemaname the name of DB schema
+#' @param tablename the name of DB table
+#' @param attname the name of the attribute
+#' @param attlen primary length of the type
+#' @param attlen2 secondary length of the type
+#' @param colposition position of the column in table
+#' @param notnull has the attribute NOT NULL constraint?
+#' @param hasdefault has the attribute default value?
+#' @param statlevel level of the statistics collected by DB
+#' @param null_frac DB-estimated proportion of NULL values
+#' @param n_distinct DB-estimated number of distinct values
+#' @param most_common_vals JSON-encoded table of most common attribute values (as estimated by DB)
+#' @param most_common_freqs JSON-encoded table of the frequencies of most common attribute values (as estimated by DB)
+#' @param histogram_bounds JSON-encoded table of the histogram bounds of attribute values
+#' @param internalid (reserved)
+#' @param internalname (reserved)
+#' @export
+db.attribute <- function(schemaname, tablename, attname, 
+                         typename, attlen = -1, attlen2 = -1, 
+                         colposition,
+                         notnull = FALSE, hasdefault = FALSE, 
+                         null_frac = NA, 
+                         n_distinct = NA, most_common_vals = NA, most_common_freqs = NA, histogram_bounds = NA,
+                         internalid, internalname
+                         ){
+  db.att.obj <- 
+  list("schemaname" = schemaname, 
+       "tablename" = tablename, 
+       "attname" = attname, 
+       "typename" = typename, 
+       "attlen" = attlen, 
+       "attlen2" = attlen2, 
+       "colposition" = colposition, 
+       "notnull" = notnull, 
+       "hasdefault" = hasdefault, 
+       "statlevel" = statlevel, 
+       "null_frac" = null_frac, 
+       "n_distinct" = n_distinct, 
+       "most_common_vals" = most_common_vals, 
+       "most_common_freqs" = most_common_freqs, 
+       "histogram_bounds" = histogram_bounds, 
+       "internalid" = internalid, 
+       "internalname" = internalname)  
+  
+  class(db.att.obj) <- "db.attribute"
+}
+
+
 
